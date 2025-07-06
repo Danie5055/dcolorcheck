14abbbfe7cba6f55b217324fdc671b3f67927c55
Danie5055 <danieldushimire@gmail.com>
2025-07-06 08:24:15 +0200

    Updated something

--- app.py
+++ app.py
@@ -225,8 +225,13 @@ def index():
     return render_template('index.html', image=image_filename, colors=colors)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
 
 
 
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
